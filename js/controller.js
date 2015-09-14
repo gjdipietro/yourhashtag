743c31b86ed173e6ce62163c681034ba29ef6716
unknown <gjdipietro@gmail.com>
2015-09-14 17:09:08 -0400

    logs everywehree

--- js/controller.js
+++ js/controller.js
@@ -5,6 +5,9 @@ angular.module('hashtagCtrl', [])
 .controller('hashtagCtrl', ['$scope', 'igAPI',
   function($scope, igAPI) {
     function getIgData(hashtag) {
+
+      console.log("laeast i'm here");
+
       igAPI.getPhotoData(hashtag)
         .success(function (data) {
           console.log("hashtag: " + hashtag);
