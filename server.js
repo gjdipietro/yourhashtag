1f39c10b727a8a6887311518f55c06d8c43fe08d
unknown <gjdipietro@gmail.com>
2015-09-15 11:52:03 -0400

    removed app views

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ var port = process.env.PORT || 1337;
 
 
 // Express Middleware
-app.set('views', path.join(__dirname, 'app/views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function (req, res, next) {
   next();
@@ -17,4 +16,3 @@ app.use(function (req, res, next) {
 
 // Start Server
 app.listen(port);
-console.log('APP is running on port ' + port);
