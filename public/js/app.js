572710d9ce13b4456236bdae9c85c7093492dbfc
unknown <gjdipietro@gmail.com>
2015-09-22 16:21:41 -0400

    added caption and title

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,26 @@
 'use strict';
+/*jslint browser:true */
 
 var app = angular.module('hashtagApp', [
   'hashtagCtrl',
-  'instagramService'
+  'instagramService',
 ]);
 
 app.constant('instagramApiConfig', {
-  apiUrl: 'https://api.instagram.com/v1/',
-  clientId: '93d7311401a74e1b8c1a3b9fa196ea20',
-  secret: '5499973fbd254dccb72c4e3707803f32'
+	apiUrl: 'https://api.instagram.com/v1/',
+ 	clientId: '93d7311401a74e1b8c1a3b9fa196ea20',
+	secret: '5499973fbd254dccb72c4e3707803f32'
 });
 
 app.run(['instagramAPI', 'instagramApiConfig', function (instagramAPI, instagramApiConfig) {
-  instagramAPI.setCredentials(instagramApiConfig);
+	instagramAPI.setCredentials(instagramApiConfig);
 }]);
 
+
+app.config(['$locationProvider', function ($locationProvider) {
+	if (window.history && window.history.pushState) {
+        $locationProvider.html5Mode(true);
+    } else {
+        $locationProvider.html5Mode(false);
+    }
+}]);
--- public/js/controller.js
+++ public/js/controller.js
@@ -4,21 +4,27 @@
 angular.module('hashtagCtrl', [])
 .controller('hashtagCtrl', ['$scope', 'instagramAPI',
   function($scope, instagramAPI) {
-    //set initial value
-    $scope.hashtag = 'gbxoxo';
+    $scope.data = {};
+    $scope.data.meta = {};
+    
     instagramAPI.setAuth();
-
+    
     $scope.processForm = function() {
-      //make API call
-      instagramAPI.fetchHashtag($scope.hashtag, function(data) {
-        $scope.images = data.map(function(x) { return x.images.standard_resolution; });
+      instagramAPI.fetchHashtag($scope._hashtag, function(data) {
+        $scope.data.images = data.map(function (x) {
+          return x.images.standard_resolution;
+        });
       });
-    }
+      $scope.data.meta.title = '#' + $scope._hashtag;
+
+    };
+
     $scope.removeImage = function (index) {
-      $scope.images.splice(index, 1);
-    }
+      $scope.data.images.splice(index, 1);
+    };
+
     $scope.featureImage = function (index, isFeatured) {
-      $scope.images[index].isFeatured = !isFeatured;
-    }
+      $scope.data.images[index].isFeatured = !isFeatured;
+    };
   }
 ]);
