572bd70eb304efd672cabcff0754e0cbe351066c
Greg DiPietro <gjdipietro@gmail.com>
2015-11-08 19:41:46 -0500

    implement john papa style guide

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,31 @@
-'use strict';
-/*jslint browser:true */
+(function() {
+  'use strict';
+  /*jslint browser:true */
 
-var app = angular.module('hashtagApp', [
-  'hashtagCtrl',
-  'instagramService',
-  'ngCookies'
-]);
+  angular
+    .module('hashtagApp', [
+      'hashtagCtrl',
+      'instagramService',
+      'ngCookies'
+    ])
+    .constant('instagramApiConfig', {
+      apiUrl: 'https://api.instagram.com/v1/',
+      clientId: '93d7311401a74e1b8c1a3b9fa196ea20',
+      secret: '5499973fbd254dccb72c4e3707803f32',
+      callback: 'http://localhost:1337/callback.html'
+    })
+    .run(['instagramAPI', 'instagramApiConfig', SetCredentials])
+    .config(['$locationProvider', setLocationProvider]);
 
-app.constant('instagramApiConfig', {
-  apiUrl: 'https://api.instagram.com/v1/',
-  clientId: '93d7311401a74e1b8c1a3b9fa196ea20',
-  secret: '5499973fbd254dccb72c4e3707803f32',
-  callback: 'http://localhost:1337/callback.html'
-});
+    function SetCredentials (instagramAPI, instagramApiConfig) {
+      instagramAPI.setCredentials(instagramApiConfig);
+    }
 
-app.run(['instagramAPI', 'instagramApiConfig', function (instagramAPI, instagramApiConfig) {
-  instagramAPI.setCredentials(instagramApiConfig);
-}]);
-
-app.config(['$locationProvider', function ($locationProvider) {
-  if (window.history && window.history.pushState) {
-    $locationProvider.html5Mode(true);
-  } else {
-    $locationProvider.html5Mode(false);
-  }
-}]);
-
+    function setLocationProvider ($locationProvider) {
+      if (window.history && window.history.pushState) {
+        $locationProvider.html5Mode(true);
+      } else {
+        $locationProvider.html5Mode(false);
+      }
+    }
+  })();
