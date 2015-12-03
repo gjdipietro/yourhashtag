ea70d504904d0fc0eed15965e9d790a6360731d4
unknown <gjdipietro@gmail.com>
2015-12-03 17:45:27 -0500

    working on save all operation

--- public/js/controller.js
+++ public/js/controller.js
@@ -16,21 +16,24 @@ function HashtagCtrl ($location, instagramAPI) {
   vm.hashtag = 'gbxoxo';
   vm.authLink = instagramAPI.getAuthLink();
   vm.hasAccessToken = instagramAPI.hasAccessToken();
+  
   //button click handlers
   vm.processForm = processForm;
   vm.clearForm = clearForm;
   vm.removeImage = removeImage;
   vm.featureImage = featureImage;
   vm.saveCollage = saveCollage;
+  vm.downloadAllImages = downloadAllImages;
+  
   //init
   instagramAPI.setAuth();
-
   if ($location.$$path !== '/') {
     var accessToken = $location.$$path.slice(1);
     instagramAPI.setAuth(accessToken);
     vm.hasAccessToken = instagramAPI.hasAccessToken();;
     $location.path('/');
   }
+  
   //App Logic
   function processForm (hashtag) {
     instagramAPI.fetchHashtag(hashtag,  function(data) {
@@ -56,6 +59,47 @@ function HashtagCtrl ($location, instagramAPI) {
 
   function featureImage (index, isFeatured) {
     vm.data.images[index].isFeatured = !isFeatured;
+  }
+
+  function downloadAllImages(images) {
+    var urls = [
+      'https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12345895_1699198440317380_418577934_n.jpg',
+      'https://scontent.cdninstagram.com/hphotos-xat1/t51.2885-15/s640x640/sh0.08/e35/12144228_1660648204205478_424448884_n.jpg',
+      'https://scontent.cdninstagram.com/hphotos-xpa1/t51.2885-15/s640x640/sh0.08/e35/11939465_867846576659579_409069286_n.jpg',
+      'https://scontent.cdninstagram.com/hphotos-xpf1/t51.2885-15/e15/11910536_1664718590477001_2113943493_n.jpg'];
+    var zip = new JSZip();
+    var canvas = document.createElement("canvas");
+    var context = canvas.getContext('2d');
+    var i;
+    var blob;
+
+    for (i = 0; i < urls.length; i++) {
+      convertImgToDataURLviaCanvas(urls[i], function(base64Img) {
+        console.log(base64Img);
+        zip.file('image-' +  i, base64Img, {base64: true});
+      });
+    }
+    window.setTimeout(function(){
+      blob = zip.generate({type:'blob'});
+      saveAs(blob, vm.hashtag + '.zip');
+    }, 10000);
+    
+    function convertImgToDataURLviaCanvas(url, callback) {
+      var img = new Image();
+      img.crossOrigin = 'Anonymous';
+      img.onload = function() {
+          var canvas = document.createElement('canvas');
+          var ctx = canvas.getContext('2d');
+          var dataURL;
+          canvas.height = this.height;
+          canvas.width = this.width;
+          ctx.drawImage(this, 100, 100);
+          dataURL = canvas.toDataURL('image/jpeg', 1.0);
+          callback(dataURL);
+          canvas = null;
+      };
+      img.src = url;
+    }
   }
 
   function saveCollage (data) {
