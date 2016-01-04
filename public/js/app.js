4ba7f44447c0430392f4030136737034a829612b
unknown <gjdipietro@gmail.com>
2016-01-04 17:15:30 -0500

    testing ghpages config

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@
     clientId: '93d7311401a74e1b8c1a3b9fa196ea20',
     secret: '5499973fbd254dccb72c4e3707803f32',
     devcallback: 'http://localhost:1337/callback.html',
-    callback: 'http://gjdipietro.github.io/callback.html'
+    callback: 'http://gjdipietro.github.io/yourhashtag/public/callback.html'
   };
 
   angular
