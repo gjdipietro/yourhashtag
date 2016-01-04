28a71948940ab6a14473895fe4853c4d55086621
unknown <gjdipietro@gmail.com>
2016-01-04 17:12:16 -0500

    added devcallback

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,8 @@
     apiUrl: 'https://api.instagram.com/v1/',
     clientId: '93d7311401a74e1b8c1a3b9fa196ea20',
     secret: '5499973fbd254dccb72c4e3707803f32',
-    callback: 'http://localhost:1337/callback.html'
+    devcallback: 'http://localhost:1337/callback.html',
+    callback: 'http://gjdipietro.github.io/callback.html'
   };
 
   angular
