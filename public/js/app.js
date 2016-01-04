ad4dccf99b2180dcc2b7d761cdd676d234b69739
unknown <gjdipietro@gmail.com>
2016-01-04 17:20:01 -0500

    reverted ghpages congif

--- public/js/app.js
+++ public/js/app.js
@@ -6,8 +6,7 @@
     apiUrl: 'https://api.instagram.com/v1/',
     clientId: '93d7311401a74e1b8c1a3b9fa196ea20',
     secret: '5499973fbd254dccb72c4e3707803f32',
-    devcallback: 'http://localhost:1337/callback.html',
-    callback: 'http://gjdipietro.github.io/yourhashtag/public/callback.html'
+    callback: 'http://localhost:1337/callback.html',
   };
 
   angular
