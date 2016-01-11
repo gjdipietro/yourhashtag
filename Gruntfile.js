3b13770e49db275e25e0c33dbd650a00f4f13d78
unknown <gjdipietro@gmail.com>
2016-01-11 16:11:13 -0500

    added a few grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,44 @@
 module.exports = function(grunt) {
 
-    // 1. All configuration goes here 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-    });
+  // 1. All configuration goes here 
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-    // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-clean');
+    watch: {
+		  livereload: {
+	      options: { 
+	      	livereload: true 
+	      },
+	      files: ['build/**/*'],
+			},
+		},
 
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['php:server', 'watch']); //everyday work
+    autoprefixer: {
+      options: {
+				browsers: ['last 2 versions', 'ie 8', 'ie 9', '> 1%']
+      },
+      main: {
+        expand: true,
+        flatten: true,
+        src: 'public/assets/css/*.css',
+        dest: 'build/'
+      }
+    },
+
+    clean: ["build/"]
+
+  });
+
+  // 3. Where we tell Grunt we plan to use this plug-in.
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+  grunt.registerTask('default', ['watch']); //everyday work
+  grunt.registerTask('build', ['clean', 'autoprefixer']); // push to live
+
 };
---
+++ config.rb
@@ -0,0 +1,10 @@
+preferred_syntax = :scss
+http_path = '/'
+css_dir = 'public/assets/css'
+sass_dir = 'public/assets/scss'
+images_dir = 'public/assets/img'
+javascripts_dir = 'public/assets/js'
+output_style = :compressed
+relative_assets = true 
+line_comments = true
+environment = :development 
